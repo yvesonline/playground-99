8ff7623fba31432e8cfa6cf1d43e860c3973fcc5
Yves Weissig <yves@yvesonline.de>
2018-11-03 14:44:45 +0000

    Fixed Free Pascal example mistakes

--- freepascal/99.pas
+++ freepascal/99.pas
@@ -1,11 +1,12 @@
-program NinetyNine;
-var
+Program NinetyNine;
+Uses sysutils;
+Var
   i: Integer;
-begin
-  for i := 100 to 1 do
+Begin
+  For i := 100 downto 1 do
   begin
-    WriteLn(i, ' bottles of beer on the wall, ', i, ' bottles of beer');
-    WriteLn('Take one down and pass it around, ', i-1, ' bottles of beer on the wall');
+    WriteLn(IntToStr(i) + ' bottles of beer on the wall, ' + IntToStr(i) + ' bottles of beer');
+    WriteLn('Take one down and pass it around, ' + IntToStr(i-1) + ' bottles of beer on the wall');
   end;
-  readln;
-end.
+  ReadLn;
+End.
