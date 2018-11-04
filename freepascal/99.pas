b832ef19bfc17f47b867d1dfe2839215fc99cf87
Yves Weissig <yves@yvesonline.de>
2018-11-04 12:52:25 +0000

    Fixed Free Pascal implementation, was starting at 100

--- freepascal/99.pas
+++ freepascal/99.pas
@@ -3,7 +3,7 @@ Uses sysutils;
 Var
   i: Integer;
 Begin
-  For i := 100 downto 1 do
+  For i := 99 downto 1 do
   begin
     WriteLn(IntToStr(i) + ' bottles of beer on the wall, ' + IntToStr(i) + ' bottles of beer');
     WriteLn('Take one down and pass it around, ' + IntToStr(i-1) + ' bottles of beer on the wall');
