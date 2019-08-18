3b51f286ac002ad5cd638f55f426fc5c608850b5
Yves Weissig <yves@yvesonline.de>
2019-08-18 14:37:45 +0100

    Added Python implementation

---
+++ python/99.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+for i in range(99, 0, -1):
+    print(f"{i} bottles of beer on the wall, {i} bottles of beer")
+    print(f"Take one down and pass it around, {i-1} bottles of beer on the wall")
