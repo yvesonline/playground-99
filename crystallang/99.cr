1cc5bcce6390a6c4d32e9b07b0610024ad6246b3
Yves Weissig <yves@yvesonline.de>
2020-04-23 15:22:44 +0100

    Adding Crystal implementation

---
+++ crystallang/99.cr
@@ -0,0 +1,7 @@
+cnt = 99
+
+while cnt > 0
+	puts "#{cnt} bottles of beer on the wall, #{cnt} bottles of beer"
+	puts "Take one down and pass it around, #{cnt - 1} bottles of beer on the wall"
+	cnt -= 1
+end
