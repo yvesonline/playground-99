742da8c5deeaa5baf95b7d9e84413ee3eb588126
Yves Weissig <yves@weissig.me>
2023-08-05 11:49:24 +0100

    Added Nim implementation

---
+++ nimlang/beers.nim
@@ -0,0 +1,6 @@
+var num: int = 99
+
+while num > 0:
+  echo(num, " bottles of beer on the wall, ", num, "bottles of beer")
+  num = num - 1
+  echo("Take one down and pass it around, ", num, " bottles of beer on the wall")
